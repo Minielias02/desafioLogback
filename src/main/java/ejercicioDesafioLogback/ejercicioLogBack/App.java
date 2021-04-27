e461dcc15367d0e9706162ba3f7886405e05354f
Elias <eespinoh@everis.com>
2021-04-27 22:24:13 +0200

    Proyecto de logs SuperChulo

--- src/main/java/ejercicioDesafioLogback/ejercicioLogBack/App.java
+++ src/main/java/ejercicioDesafioLogback/ejercicioLogBack/App.java
@@ -1,5 +1,12 @@
 package ejercicioDesafioLogback.ejercicioLogBack;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,18 +18,64 @@ import org.slf4j.LoggerFactory;
  */
 public class App {
 	final static Logger LOGGER = LoggerFactory.getLogger(App.class);
-	
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		LOGGER.info("Comienza el metodo con un info");
-
-		for (int i=0; i<5000;i++) {
-			LOGGER.debug("Esto es un debug");
-			LOGGER.error("Esto es un error");
-		}
-		LOGGER.trace("Este no se muestra porque esta por encima ");
-		LOGGER.warn("Esto es un warning");
 		
+		//Este metodo escribe todas las lineas que quieras, si quieres que haga
+		// archivos zip deberas de poner mas 1.000 repeticiones
+		try (FileWriter lineWritter= new FileWriter("muchasLineas.txt");	                
+				Scanner op=new Scanner(System.in)) {
+	            LOGGER.info("Comienzo del programa");
+	            System.out.println("Escriba cuantos numeros quiere escribir");
+	            int repetitions=op.nextInt();
+	            //Este es el escritor en un archivo
+	            for (int i=0; i<repetitions;i++) {
+		            lineWritter.write(i+"\n");
+	    		}
+	        } catch (FileNotFoundException e) {
+	            LOGGER.error("No encuentra el fichero 1");
 
+	        } catch (IOException e1) {
+	            LOGGER.error("Error padre de File");
+
+	        }
+			//Aqui leo el archivo y luego lo mando todo a las trazas
+			//Hay sysouts para comprobar por consola como iba
+		  try(FileReader in = new FileReader("muchasLineas.txt");
+	                BufferedReader reader = new BufferedReader(in);
+	                Scanner op2=new Scanner(System.in)
+	                ) {
+
+	            String texto= reader.readLine();
+	            // counter de las paginas
+	            int counter=0;
+	            //Lee 100 lineas cada vez hasta que no encuentre mas nada
+	            while(texto!=null) {
+	                String fileNumbers="";
+	                for(int i=0;i<=100;i++) {
+	                    // Mira si queda texto o no.
+	                    if(texto!=null) {
+	                        fileNumbers =fileNumbers+"\n"+texto;
+	                        texto=reader.readLine();
+	                        counter++;
+	                    }
+	                }
+	                LOGGER.debug(fileNumbers);
+	                LOGGER.info("Se han leido: "+(counter)+" lineas");
+	                LOGGER.info("-----------------------------------------------");
+	                System.out.println(fileNumbers);
+	                System.out.println("Se han leido: "+(counter)+" lineas");
+	                System.out.println("-----------------------------------------------");
+	            }
+	            LOGGER.info("Fin del programa Tellez");
+	        } catch (FileNotFoundException e) {
+	            LOGGER.error("No encuentra el fichero 1");
+	        } catch (IOException e) {
+	            LOGGER.error("Error padre de File");
+	        }
+	    }
 	}
-}
+
